5f36aff7cd7d14b87b366221cdd3f596bba9c077
Ddevender Saini <devender03saini@gmail.com>
2026-03-28 22:33:24 +0530

    Delete ERP/client directory

--- ERP/client/src/components/atteandance_tooglebar.jsx
+++
@@ -1,7 +0,0 @@
-function Toogle_bar(){
-    return(
-        <h1>Toogle Bar</h1>
-    )
-}
-
-export default Toogle_bar
--- ERP/client/src/pages/laundary.jsx
+++
@@ -1,5 +0,0 @@
-function Laundary(){
-    return <h1>Laundary</h1>
-}
-
-export default Laundary
