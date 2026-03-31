39da7fe42ee31eede88d2c54d0fd77edb572275c
Ddevender Saini <devender03saini@gmail.com>
2026-03-31 15:49:15 +0530

    TImetable c

--- ERP/backend/routes/timetableRoutes.js
+++ ERP/backend/routes/timetableRoutes.js
@@ -9,23 +9,18 @@ router.get("/test", (req, res) => {
     res.send("Timetable route working");
 });
 
-// GET /api/timetable
 router.get("/", protect, async (req, res) => {
     try {
-        //YOUR middleware gives full user object
         const userId = req.user._id;
 
-        //Find student linked to this user
         const student = await Student.findOne({ user: userId });
 
         if (!student) {
             return res.status(404).json({ message: "Student not found" });
         }
 
-        //Extract class info
         const { branch, section, group } = student;
 
-        //Find timetable
         const timetable = await Timetable.findOne({
             branch,
             section,
@@ -36,7 +31,6 @@ router.get("/", protect, async (req, res) => {
             return res.status(404).json({ message: "Timetable not found" });
         }
 
-        // Sends responsse
         res.json({
             timeSlots: timetable.timeSlots,
             timetable: timetable.timetable
